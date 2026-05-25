17793172a2d4eb7f13e854ed17182ca4f9e85dd2
sr0302-lab <sakisaki4848@gmail.com>
2026-05-25 15:24:35 +0900

    object.cppのコードを初期状態に戻す

--- object.cpp
+++ object.cpp
@@ -12,52 +12,28 @@ using namespace gg;
 //   index: 線分の頂点インデックス
 //   戻り値: 作成された頂点配列オブジェクト名
 //
-// 以前のコード
-// GLuint createObject(GLuint vertices, const GLfloat(*position)[3], GLuint lines, const GLuint* index)
-// 追加コード
-GLuint createObject(
-    GLuint vertices,
-    const GLfloat(*position0)[3],
-    const GLfloat(*position1)[3],
-    GLuint lines,
-    const GLuint* index
-)
-//
+GLuint createObject(GLuint vertices, const GLfloat(*position)[3], GLuint lines, const GLuint* index)
 {
   // 頂点配列オブジェクト
   GLuint vao;
   glGenVertexArrays(1, &vao);
   glBindVertexArray(vao);
 
-  // 追加コード2
-  // p0用の頂点バッファオブジェクト
-  GLuint vbo0;
-  glGenBuffers(1, &vbo0);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo0);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat[3]) * vertices, position0, GL_STATIC_DRAW);
-
-  // layout(location = 0)のp0に送る
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  glEnableVertexAttribArray(0);
-  //
-
-  // 追加コード3
-  // p1用の頂点バッファオブジェクト
-  GLuint vbo1;
-  glGenBuffers(1, &vbo1);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo1);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat[3]) * vertices, position1, GL_STATIC_DRAW);
-
-  // layout(location = 1)のp1に送る
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  glEnableVertexAttribArray(1);
-  //
+  // 頂点バッファオブジェクト
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat[3]) * vertices, position, GL_STATIC_DRAW);
 
   // インデックスバッファオブジェクト
   GLuint ibo;
   glGenBuffers(1, &ibo);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * lines, index, GL_STATIC_DRAW);
+
+  // 結合されている頂点バッファオブジェクトを in 変数から参照できるようにする
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(0);
 
   // 頂点配列オブジェクトの結合を解除した後に頂点バッファオブジェクトとインデックスバッファオブジェクトの結合を解除する
   glBindVertexArray(0);
